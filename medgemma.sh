94957a0ce1d1bd6f8438657d9bdc126ac3eb6acd
Andrew Lian <andrewlian345@gmail.com>
2026-02-23 20:14:01 -0700

    change to relative paths

--- medgemma.sh
+++ medgemma.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 #SBATCH --job-name=medgemma
+#SBATCH -N 1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mem=50G
+
+# optional
+#SBATCH --partition=gpu-v100
 
 #SBATCH --output=.cache/medgemma/medgemma-%j.out
 #SBATCH --error=.cache/medgemma/medgemma-%j.err
 
 export MEDGEMMA_DEVICE=cuda
 
-source .venv/medgemma/bin/activate
-echo "VIRTUAL_ENV=$VIRTUAL_ENV"
+# source .venv/medgemma/bin/activate
+# echo "VIRTUAL_ENV=$VIRTUAL_ENV"
+
+source $HOME/.bashrc
+conda activate medgemma
+
 which python
 
 
